b13f54b17da1a4dba113223bb8b24ad9cc5da161
haim-ai <230439031+haim-ai@users.noreply.github.com>
2026-09-21 21:55:22 +0300

    Deploy static export

---
+++ _next/static/chunks/33kz4he7200r-.js
@@ -0,0 +1,1 @@
+(globalThis.TURBOPACK||(globalThis.TURBOPACK=[])).push(["object"==typeof document?document.currentScript:void 0,29123,t=>{t.v(o=>Promise.all(["static/chunks/0nq_uxsf7z95a.js"].map(o=>t.l(o))).then(()=>o(82820)))}]);
